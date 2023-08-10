ff4c257f99bc1240c6f7a74c311290af1fc3b7b3
Paurush Sinha <paurush.sinha.d@gmail.com>
2023-08-11 01:18:14 +0530

    Added List Item Selection Feature Added Option Bar in home.jsx Added List Item Selection functionality Added Checkbox Package Added some complex states

--- src/comp/record-item.js
+++ src/comp/record-item.js
@@ -1,9 +1,23 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import React, {useState} from 'react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import STRING from '../../constants';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import CheckBox from '@react-native-community/checkbox';
 
-export default function RecordItem({amount, remarks, credit}) {
+const CHECKBOX_TINT = {
+  true: STRING.colors.white,
+  false: STRING.colors.white,
+};
+export default function RecordItem({
+  id,
+  amount,
+  remarks,
+  credit,
+  onLongPress,
+  selectionMode,
+  selectedItems,
+  setSelectedItems,
+}) {
   const [showRemark, setshowRemark] = useState(false);
 
   let amount_ = amount.toLocaleString('en-IN', {
@@ -15,16 +29,42 @@ export default function RecordItem({amount, remarks, credit}) {
   function updateState() {
     setshowRemark(!showRemark);
   }
+  function updateSelection() {
+    if (selectedItems.indexOf(id) === -1) {
+      setSelectedItems(currentValue => {
+        let namespace = currentValue;
+        namespace.push(id);
+        return [...namespace];
+      });
+    } else {
+      setSelectedItems(currentValue => {
+        let namespace = currentValue;
+        namespace.pop(namespace.indexOf(id));
+        return [...namespace];
+      });
+    }
+  }
+  const blankRemark = remarks.replaceAll(' ', '') === '';
+  remarks = blankRemark ? 'No Detail Provided' : remarks;
 
   return (
     <View style={style.item}>
-      <TouchableOpacity onPress={updateState}>
+      <Pressable onPress={updateState} onLongPress={onLongPress}>
         <View style={[style.itemHead, style.flexGap]}>
           <View style={style.itemHead}>
-            <MaterialIcons
-              name="account-balance-wallet"
-              style={[style.icon, credit ? style.green : style.red]}
-            />
+            {selectionMode ? (
+              <CheckBox
+                value={selectedItems.indexOf(id) !== -1}
+                onChange={updateSelection}
+                tintColors={CHECKBOX_TINT}
+              />
+            ) : (
+              <MaterialIcons
+                name="account-balance-wallet"
+                style={[style.icon, credit ? style.green : style.red]}
+              />
+            )}
+
             <Text style={[style.amount, credit ? style.green : style.red]}>
               {credit ? '+' : '-'} {amount_}
             </Text>
@@ -36,7 +76,7 @@ export default function RecordItem({amount, remarks, credit}) {
             />
           </View>
         </View>
-      </TouchableOpacity>
+      </Pressable>
       {showRemark ? <Text style={style.remarks}>{remarks}</Text> : null}
     </View>
   );
@@ -48,7 +88,7 @@ const style = StyleSheet.create({
     backgroundColor: STRING.colors.grey,
     alignSelf: 'center',
     padding: 5,
-    borderRadius: 5,
+    borderRadius: 8,
     justifyContent: 'center',
     borderColor: STRING.colors.white,
     borderWidth: 1,
--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,30 +1,119 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from '../comp/header';
 import Scaffold from '../comp/scaffold';
-import {FlatList} from 'react-native';
+import {
+  Alert,
+  FlatList,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from 'react-native';
 import RecordItem from '../comp/record-item';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import STRING from '../../constants';
 
 const ScreenHeader = () => <Header title={'Past\nExpenses'} />;
 
-function itemToRender({item}) {
-  return <RecordItem {...item} />;
-}
-
 export default function Home() {
+  const [selectionMode, setSelectionMode] = useState(false);
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  function onLongPress() {
+    if (selectionMode !== true) {
+      setSelectionMode(true);
+    }
+  }
+
+  function dismissSelectMode() {
+    setSelectionMode(false);
+    setSelectedItems([]);
+  }
+  function deleteItems() {
+    if (selectedItems.length > 0) {
+      Alert.alert('Niska Asks', 'Are you sure to delete selected items?', [
+        {
+          text: 'Yes, Delete',
+          onPress: () => alert('Deleted'),
+        },
+        {
+          text: "No, Don't Delete",
+        },
+      ]);
+    }
+  }
+
+  function itemToRender({item}) {
+    return (
+      <RecordItem
+        {...item}
+        onLongPress={onLongPress}
+        selectionMode={selectionMode}
+        selectedItems={selectedItems}
+        setSelectedItems={setSelectedItems}
+      />
+    );
+  }
   return (
     <Scaffold flatListCompatible={true}>
+      {selectionMode ? (
+        <View style={style.options}>
+          <TouchableOpacity style={style.optionButton} onPress={deleteItems}>
+            <MaterialIcons
+              name="delete"
+              style={[style.optionIcon, style.red]}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={style.optionButton}
+            onPress={dismissSelectMode}>
+            <MaterialIcons
+              name="clear"
+              style={[style.optionIcon, style.white]}
+            />
+          </TouchableOpacity>
+        </View>
+      ) : null}
+
       <FlatList
         ListHeaderComponent={ScreenHeader}
         renderItem={itemToRender}
+        keyExtractor={item => item.id}
         data={[
           {
             amount: 500,
             credit: true,
-            remarks: '',
+            remarks: 'Hello world',
             id: '1',
+          },
+          {
+            amount: 500,
+            credit: true,
+            remarks: '    ',
+            id: '2',
           },
         ]}
       />
     </Scaffold>
   );
 }
+
+const style = StyleSheet.create({
+  options: {
+    backgroundColor: STRING.colors.primary,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    padding: 15,
+  },
+  optionIcon: {
+    fontSize: 30,
+  },
+  optionButton: {
+    margin: 10,
+  },
+  white: {
+    color: STRING.colors.white,
+  },
+  red: {
+    color: STRING.colors.red,
+  },
+});
